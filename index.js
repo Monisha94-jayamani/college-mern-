614424a42772d9f854ff44f586a0e55203eb514d
unknown <monijacklein@gmail.com>
2020-07-17 00:31:09 -0700

    hi

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express();
 const boyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(cors());
 app.use(boyParser.json());
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
-//const dbUrl = "mongodb://127.0.0.1:27017/";
-//password:4QJnwN3i7wN0jWGB
-//url:mongodb+srv://monisha:4QJnwN3i7wN0jWGB@cluster0.a3jmi.mongodb.net/student?retryWrites=true&w=majority
 const dbUrl =
   "mongodb+srv://monisha:4QJnwN3i7wN0jWGB@cluster0.a3jmi.mongodb.net/student?retryWrites=true&w=majority";
+//password:4QJnwN3i7wN0jWGB
+//url:mongodb+srv://monisha:4QJnwN3i7wN0jWGB@cluster0.a3jmi.mongodb.net/student?retryWrites=true&w=majority
+//const url =
+//"mongodb+srv://monisha:4QJnwN3i7wN0jWGB@cluster0.a3jmi.mongodb.net/student?retryWrites=true&w=majority";
+
 const ObjectID = mongodb.ObjectID;
 app.get("/staff", (req, res) => {
   mongoClient.connect(dbUrl, (err, client) => {
